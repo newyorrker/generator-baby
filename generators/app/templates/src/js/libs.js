4c60c7e21b4fac8fd3cd1b428dd520015c5d7c5a
Newyorrker <newyorrker@gmail.com>
2018-05-09 23:38:46 +0300

    update js files for bundler

---
+++ generators/app/templates/src/js/libs.js
@@ -0,0 +1,2 @@
+global.jQuery = require("jquery");
+// import slick from "slick-carousel";
--- generators/app/templates/src/js/main.js
+++ generators/app/templates/src/js/main.js
@@ -1,18 +1,13 @@
+require('./libs.js');
+
 jQuery(document).ready(function($) {
 
-	//
+	console.log('hello world');
 
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-	//
+	console.log('hello world vanilla.js');
 
 });
-
-
-
-
-
-
-
