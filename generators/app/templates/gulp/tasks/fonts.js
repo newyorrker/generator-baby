c56c1737c97906e72c8fad30ec1192173dbe08c0
Newyorrker <newyorrker@gmail.com>
2018-06-05 08:24:33 +0300

    update fonts task

--- generators/app/templates/gulp/tasks/fonts.js
+++ generators/app/templates/gulp/tasks/fonts.js
@@ -1,32 +1,13 @@
 const gulp 						= require('gulp');
 const config       		= require('../config');
-const runSequence 		= require('run-sequence');
-const watch 					= require('gulp-watch');
-const googleWebFonts 	= require('gulp-google-webfonts');
-
-var options = {
-	fontsDir: '../fonts',
-	cssDir: '../../src/sass/',
-	cssFilename: '_g-fonts.scss'
-};
 
 //--------------------------------------------------------------
 // FONTS TASK
 //--------------------------------------------------------------
 
-gulp.task('fonts', function(cb) {
-    runSequence('google-fonts', 'font-copy');
-});
-
-gulp.task('google-fonts', function () {
-	return gulp.src(config.src.fonts + '/fonts.list')
-	.pipe(googleWebFonts(options))
-	.pipe(gulp.dest(config.dest.fonts));
-});
-
 //for custom fonts generate css from transfonter.org
-gulp.task('font-copy', function () {
-  return gulp.src(config.src.fonts + '/**/*[^.list]')
+gulp.task('fonts', function () {
+  return gulp.src(config.src.fonts + '/**/*.*')
     .pipe(gulp.dest(config.dest.fonts));
 });
 
